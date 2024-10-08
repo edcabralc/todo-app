8fa20d2ac4aeadf4adaa84d4eb754cc820b25662
edcabralc <cabralnet@gmail.com>
2024-10-08 15:47:07 -0300

    refact: implementação dos hooks useTodos e useFilter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import { useReducer } from "react";
-
 import { TodoType } from "@/data/@types/todo.type";
+import { useTodos } from "@/hooks/useTodos";
 
 import { Card } from "@/components/ui/card";
 import { Footer } from "@/components/footer";
@@ -10,25 +9,13 @@ import { TodoItem } from "@/components/todo-item";
 import { EmptyAlert } from "@/components/empty-alert";
 import { Todo } from "@/components/todo";
 import { AddTodo } from "@/components/add-todo";
-import { todoReducer } from "@/reducers/todo.reducer";
+import { useFilter } from "@/hooks/useFilter";
 
 const Home = () => {
-  const [items, dispatch] = useReducer(todoReducer, []);
+  const { items, addTodo, removeTodo, markIsDone } = useTodos();
+  const { applyFilter, setFilter } = useFilter();
 
   const totalItemsTodo = items.length;
-
-  const handleAddTodoItems = (item: TodoType) => {
-    const newTask = item;
-    dispatch({ type: "add", payload: newTask });
-  };
-
-  const handleDeleteTodo = (id: string) => {
-    dispatch({ type: "remove", payload: { id } });
-  };
-
-  const handleDoneTodo = (id: string) => {
-    dispatch({ type: "done", payload: { id } });
-  };
 
   return (
     <main className="flex min-h-screen flex-col items-center p-6 gap-4 bg-gradient-to-b from-sky-400 to-indigo-700">
@@ -36,28 +23,35 @@ const Home = () => {
       <Card className="w-full flex flex-col space-y-8 md:w-[670px] p-6 bg-slate-100">
         <div className="flex gap-4 items-center">
           <div className="w-full flex gap-2">
-            <AddTodo onAddTodo={handleAddTodoItems} />
+            <AddTodo onAddTodo={addTodo} />
           </div>
         </div>
         <Todo>
           <>
-            {items.length <= 0 && (
+            {applyFilter(items).length <= 0 && (
               <EmptyAlert>
                 Você ainda não tem nenhuma tarefa. Adicione alguma tarefa 🚀.
               </EmptyAlert>
             )}
 
-            {items.map((item: any) => (
+            {applyFilter(items).map((item: any) => (
               <TodoItem
                 key={item.id}
                 item={item}
-                onDelete={handleDeleteTodo}
-                onDone={handleDoneTodo}
+                onDelete={removeTodo}
+                onDone={markIsDone}
               />
             ))}
           </>
         </Todo>
-        {totalItemsTodo <= 0 ? null : <Footer total={totalItemsTodo} />}
+        {totalItemsTodo <= 0 ? null : (
+          <Footer
+            total={totalItemsTodo}
+            onFilterAll={() => setFilter("all")}
+            onFilterDone={() => setFilter("donse")}
+            onFilterUndone={() => setFilter("undone")}
+          />
+        )}
       </Card>
     </main>
   );
